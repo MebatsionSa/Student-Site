dbfcb935ab8f25961e563cdadeb7ce1e5c362cdf
henokfekade <henokfekade2468@gmail.com>
2020-03-24 12:57:49 +0300

    index page template

--- siteapp/views.py
+++ siteapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request,
-                 'siteapp/base_site.html'
+                 'siteapp/index.html'
                   )
